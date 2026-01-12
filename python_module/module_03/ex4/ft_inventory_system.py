cc623915f279bf94270e6055caa044e65fdb1bfa
Meurer <morgane@blocked.local>
2026-01-12 23:17:49 +0100

    some methods created to print inventory

--- python_module/module_03/ex4/ft_inventory_system.py
+++ python_module/module_03/ex4/ft_inventory_system.py
@@ -2,8 +2,19 @@
 
 class InventoryMaster:
     player_inventory = []
+    data = {
+            "helmet": ("armor", "common", 400),
+            "shield": ("armor", "common", 600),
+            "potion": ("consommable", "common", 10),
+            "elixir_de_temps": ("consommable", "rare", 1000),
+            "poison": ("consommable", "uncommon", 100),
+            "sword": ("weapon", "common", 500),
+            "bow": ("weapon", "uncommon", 750),
+            "royal_bow": ("weapon", "rare", 1200),
+            }
 
     def __init__(self, name, *inventory):
+        """Inventory initalisation"""
         self.name = name
         self.inventory = {}
         for item, qty in inventory:
@@ -11,6 +22,7 @@ class InventoryMaster:
         InventoryMaster.player_inventory.append(self)
 
     def add_items(self, *inventory):
+        """get in a certain quantity of an item"""
         for item, qty in inventory:
             if item in self.inventory:
                 self.inventory[item] += qty
@@ -18,19 +30,35 @@ class InventoryMaster:
                 self.inventory[item] = qty
 
     def lose_items(self, *inventory):
+        """get off a certain quantity of an item"""
         for item, qty in inventory:
             if self.inventory[item] < qty:
                 return f'Not enough {item} in {self.name} inventory'
             else:
                 self.inventory[item] -= qty
 
+    def inside_bag(self):
+        """Print all the player inventory: type, rarity and value"""
+        print(f"=== {self.name.capitalize()}'s inventory ===")
+        for item, nbr in self.inventory.items():
+            type, rarity, value = InventoryMaster.data[item]
+            print(
+                f"{item} ({type}, {rarity}): "
+                f"{int(nbr)}x @ {value} gold each = {nbr * value} gold")
+
+    def bag_stats(self):
+        """get some stats on the inventory"""
+        print(self.inventory.get("sword"))
+        # exemple de tests pour la methode get(). A reprendre ici
+
+
+# valeur de l'inventaire du personnage, combien d'item et les categories
+# inventaire analytics
+
 
 if __name__ == "__main__":
-    print("=== Player Inventory System ===")
-    alice_inventory = InventoryMaster(
+    print("=== Player Inventory System ===\n")
+    alice_bag = InventoryMaster(
         'alice', ('sword', 1), ('potion', 10), ('shield', 1))
-    print(alice_inventory.inventory)
-    alice_inventory.add_items(('potion', 10))
-    print(alice_inventory.inventory)
-    alice_inventory.lose_items(('potion', 10))
-    print(alice_inventory.inventory)
+    alice_bag.inside_bag()
+    alice_bag.bag_stats()
